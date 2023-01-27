90b75bc638ebbb31f9cef8eb9cc395d9865ce996
Rita <ritucis@inbox.lv>
2023-01-27 17:40:50 +0000

    26.01.2023 Rita

---
+++ src/GameIteractions.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GameIteractions {
+
+    public static void printInstruction(){
+        System.out.println("\nThis is version of V.I.P. quiz or mind game!");
+        System.out.println("\tV.I.P. are abbreviation of Veiksme, Intuīcija, Prāts (Success, Intuition, Mind");
+        System.out.println("\t - Two teams participate in the game");
+        System.out.println("\t - Each team has the same number of questions");
+        System.out.println("\t - Value for every question is 150 points");
+        System.out.println("\t - Every team has max points for all question`s at the beginning of the game - starting score");
+        System.out.println("\t - If team gives correct answer to the question it keeps the current score");
+        System.out.println("\t - If team needs to choose some hint, it costs a certain number of points - hints value");
+        System.out.println("\t - The winner is team with biggest score\n");
+    }
+
+    public static void readData() throws SQLException {
+
+        String sql = "SELECT*FROM Result"; //Check if table name will be the same
+        Connection conn = null;
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        int count = 0;
+
+        while(resultSet.next()){
+            String teamName = resultSet.getString("fullname");
+            String score = resultSet.getString("email");
+                                                                        // If we want to show TOP5 (not every team from list), where to define length
+            System.out.println(String.format(String.valueOf(++count),teamName,score)); //using ++ we can increase by one and print out
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
+import java.util.Scanner;
+import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
 
+
+        String dbURL = "jdbc:mysql://localhost:3306/java34";
+        String username = "root";
+        String password = "Elite";
+        Scanner scanner = new Scanner(System.in);
+        char again = 'y';
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+
+            while (again == 'y') {
+                System.out.println("Choose Your next step =>");
+                System.out.println("r - read rules");
+                System.out.println("p - play the game");
+                System.out.println("t - see TOP results");
+                System.out.println("q - quit");
+
+
+                char action = scanner.nextLine().charAt(0);
+                if (action == 'p') {
+
+                    System.out.println("Team one - please, enter Your team name:");
+                    String teamOne = scanner.nextLine();
+
+                    System.out.println("Enter Your team member`s names (one line):");
+                    String teamOneMembers = scanner.nextLine();
+
+                    System.out.println("Team Two - please, enter Your team name:");
+                    String newFullName = scanner.nextLine();
+
+                    System.out.println("Enter Your team member`s names (one line):l");
+                    String newEmail = scanner.nextLine();
+
+                    //   insertData(conn,newUsername,newPassword,newFullName,newEmail);
+
+                } else if (action == 'r') {
+                    GameIteractions.printInstruction(); //Done
+                } else if (action == 't') {
+                   GameIteractions.readData();
+                } else {
+                    //  System.out.println("Invalid input!");
+                }
+
+                System.out.println("Do You want to do more action?? y/n?");
+                again = scanner.nextLine().charAt(0);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace("Sorry, something went wrong!"); // gives info about - where things went wrong
+        }
     }
 }
